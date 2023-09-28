95b3974a0bf7721d5adfca5d561e12935c5912c6
svitlana-horodylova <horodylova.sv@gmail.com>
2023-09-28 12:47:51 +0100

    додає друге вирішення завдання до другої галереї

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,7 +1,38 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+//два варіанти вирішення завдання
+
+const container = document.querySelector('.gallery');
+
+const markup = galleryItems.map(({ preview, original, description }) => `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+                data-source="${original}" <!-- Добавьте атрибут data-source -->
+            />
+        </a>
+    </li>
+`).join("");
+
+container.insertAdjacentHTML('beforeend', markup);
+
+container.addEventListener('click', onClick);
+
+function onClick(e) {
+    e.preventDefault(); 
+
+};
 
 
+let lightbox = new SimpleLightbox(".gallery a", {
+      captionsData: "alt",
+     captionDelay: 250,
+     captionPosition: "bottom",
+    
+     });
 
 
 
